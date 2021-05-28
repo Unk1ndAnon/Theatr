e126b3c56f76cff32f3bb078b6d198d22c107574
John <50644033+000magic@users.noreply.github.com>
2021-05-28 12:52:11 -0700

    Trakt API integration

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,6 +3,15 @@
 </template>
 
 <script>
+import store from "./store";
+import Trakt from "./api/trakt";
+import SwiperCore, { Autoplay, Navigation } from "swiper";
+SwiperCore.use([Autoplay, Navigation]);
+
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/lazy/lazy.scss";
+
 export default {
   name: "App",
   methods: {
@@ -10,16 +19,47 @@ export default {
       console.log(arg);
     },
   },
+  created() {
+    const traktAuth = store.state.trakt.authCode;
+    if (traktAuth) {
+      Trakt.import_token(traktAuth).then((r) => {
+        store.commit("TraktAuth", Trakt.export_token());
+
+        setTimeout(() => {
+          console.log(
+            Trakt.sync.watched({
+              type: "movies",
+            })
+          );
+
+          // TODO remove this block
+          console.log(Trakt.sync.history.get());
+          console.log(Trakt.sync.last_activities());
+          console.log(Trakt.sync.playback.get());
+
+          Trakt.scrobble.pause({
+            movie: {
+              ids: {
+                slug: "pulp-fiction-1994",
+              },
+            },
+            progress: 1,
+          });
+        }, 5000);
+      });
+    }
+  },
 };
 </script>
 
 <style lang="scss">
 body {
   font-family: Avenir, Arial, Helvetica, sans-serif;
+  font-size: 1vw;
   margin: 0;
   padding: 0;
   user-select: none;
-  background: #202020;
+  background: #141414;
   color: white;
 }
 </style>
--- src/renderer/api/trakt.js
+++ src/renderer/api/trakt.js
@@ -1,39 +1,47 @@
-const Trakt = require("trakt.tv");
-const open = require("open");
-const http = require("http");
+import store from "../store";
 
-let options = {
+const TraktTV = require("trakt.tv");
+
+const Trakt = new TraktTV({
   client_id: "abd8388ef21f4471d28de0c6b7006499815a4c0916232ee094d55b388c0ae640",
   client_secret:
     "a2ce6a4e52fa3287f31dabdfe62a6038a402e7df3f05f2fe5e41cdd64ea75a64",
-  redirect_uri: "http://localhost:8437/trakt/authorize", // defaults to 'urn:ietf:wg:oauth:2.0:oob'
-  pagination: true, // defaults to false, global pagination (see below)
-};
+  pagination: true,
+  redirect_uri: "http://localhost:8437/trakt/authorize",
+});
 
-const trakt = new Trakt(options);
-const traktAuthUrl = trakt.get_url(); // Generate Auth URL
+export class AuthenticationServer {
+  _http = require("http");
+  _server = null;
+  _port = 8437;
 
-open(traktAuthUrl);
+  constructor(cb) {
+    this._server = this._http.createServer(cb);
+  }
 
-const requestListener = (req, res) => {
-  console.log(req.url);
+  start() {
+    this._server.listen(this._port);
+  }
 
-  const code = req.url.match(/authorize\?code=(\w+)/)[1];
+  close() {
+    this._server.close();
+  }
+}
 
-  trakt.exchange_code(code).then((r) => {
-    // contains tokens & session information
-    // API can now be used with authorized requests
-    console.log(r);
+export function isAuthenticated() {
+  return store.state.trakt.authCode ? true : false;
+}
 
-    trakt.sync.history.get().then((r) => {
-      console.log(r);
-    });
-  });
-};
+export function TraktAPI(func, args = {}) {
+  switch (func) {
+    case "sync.history.get":
+      return isAuthenticated() ? Trakt.sync.history.get(args) : null;
+    case "sync.playback.get":
+      return isAuthenticated() ? Trakt.sync.playback.get(args) : null;
 
-const server = http.createServer(requestListener);
-server.listen(8437);
+    default:
+      return null;
+  }
+}
 
-console.log("Trakt OAuth listener on " + 8437);
-
-export default trakt;
+export default Trakt;
