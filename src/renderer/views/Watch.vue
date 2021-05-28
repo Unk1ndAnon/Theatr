c00d42aae9b69fa960d19b37b94ea15ff9bf956e
John <50644033+000magic@users.noreply.github.com>
2021-05-28 12:56:35 -0700

    CancelTokens support in Watch.vue

--- src/renderer/views/Watch.vue
+++ src/renderer/views/Watch.vue
@@ -20,6 +20,7 @@
 import { MEDIA, getDetails, getEpisodes } from "../api/tmdb";
 import { getFanArt } from "../api/fanart";
 import { search } from "../api/jackett";
+import { CancelToken, axios } from "../api/axios";
 
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
@@ -38,7 +39,7 @@ export default {
       episodes: {},
       fanart: null,
       sources: null,
-      source: null,
+      sourceIndex: null,
       loading: true,
       client: null,
       torrent: null,
@@ -46,10 +47,14 @@ export default {
       blob: null,
       loadingBg: "black",
       statusText: "Loading component...",
+      cancelTokens: {
+        details: CancelToken.source(),
+        fanart: CancelToken.source(),
+      },
     };
   },
   watch: {
-    source: "renderVideo",
+    sourceIndex: "renderVideo",
     details: ["fetchFanArt", "fetchSources"],
     backdrops: "startRotatingBackdrops",
   },
@@ -95,7 +100,7 @@ export default {
           const xml2js = require("xml2js").parseString;
           xml2js(r.data, (e, d) => {
             if (d.rss.channel[0].item) {
-              const items_restructured = d.rss.channel[0].item.map((o) => {
+              const items = d.rss.channel[0].item.map((o) => {
                 var restructured = {};
 
                 restructured.title = o.title.toString();
@@ -133,10 +138,14 @@ export default {
                 return restructured;
               });
 
-              this.sources = items_restructured.sort(
-                (a, b) => b.seeders - a.seeders
-              );
-              this.source = this.sources.find((s) => s.source_type == "magnet"); // Find first magnet
+              // Filter by magnet, then sort by quality exluding those that quality isn't known.
+              this.sources = items
+                .filter((s) => s.source_type == "magnet")
+                .sort((a, b) =>
+                  a.quality && b.quality ? a.quality[1] > b.quality[1] : false
+                );
+
+              this.sourceIndex = 0;
 
               this.setStatus(
                 `Found ${this.sources.length} sources by search "${
@@ -154,58 +163,22 @@ export default {
     },
     fetchFanArt() {
       this.setStatus("Fetching fan art...");
-      const { ipcRenderer } = require("electron");
 
-      const nameOrTitle = this.details.title || this.details.name || this.mi[3];
-      const releaseDate = this.details.release_date || this.mi[4];
+      getFanArt(this.details.imdb_id || this.mi[1], this.getMediaType, {
+        cancelToken: this.cancelTokens.fanart.token,
+      }).then((r) => {
+        this.fanart = r.data;
 
-      ipcRenderer.once(
-        `fanarttv-scrape-${this.details.id || this.mi[0]}`,
-        (e, data) => {
-          const json = JSON.parse(data);
-          console.log(`All FanArt results for ${nameOrTitle}`, json);
+        const backgrounds = r.data.moviebackground || r.data.showbackground;
 
-          const fanartResults = json.filter((m) => {
-            return m.year == releaseDate.split("-")[0];
-          });
-          console.log(
-            `Filtered FanArt results for ${releaseDate}`,
-            fanartResults
+        if (backgrounds) {
+          this.backdrops.push(
+            ...(r.data.moviebackground || r.data.showbackground).filter((i) => {
+              return i.lang == "";
+            })
           );
-
-          // TODO sort the fanartResults array by imageCount (in case of multiple results for year)
-
-          // TODO some fanart names are different than the names returned from TMDb
-          // TODO write an algorithm to determine similarity between two title names (thresholded)
-          if (fanartResults[0]) {
-            getFanArt(fanartResults[0].id, this.getMediaType).then((r) => {
-              this.fanart = r.data;
-
-              const backgrounds =
-                r.data.moviebackground || r.data.showbackground;
-
-              if (backgrounds) {
-                this.backdrops.push(
-                  ...(r.data.moviebackground || r.data.showbackground).filter(
-                    (i) => {
-                      return i.lang == "";
-                    }
-                  )
-                );
-              }
-            });
-          }
         }
-      );
-
-      // Send to ipcMain, which will scrape FanArtTV (bypassing CORS) for the fanart ID
-      ipcRenderer.send(
-        `scrape-fanarttv`,
-        this.details.id || this.mi[0],
-        nameOrTitle,
-        releaseDate.split("-")[0] | "",
-        this.getMediaType === "movie" ? 3 : 1
-      );
+      });
     },
     fetchDetails() {
       // Decode the `mi` param.
@@ -218,8 +191,11 @@ export default {
       this.setStatus("Fetching details...");
 
       getDetails(this.mi[0], this.mi[2], {
-        append_to_response: "images",
-        language: "",
+        params: {
+          append_to_response: "images",
+          language: "",
+        },
+        cancelToken: this.cancelTokens.details.token,
       }).then((r) => {
         this.details = r.data;
 
@@ -263,76 +239,116 @@ export default {
         console.log("client", t);
       });
 
-      console.log("Adding", this.source.source);
+      console.log("Adding", this.sources[this.sourceIndex].source);
 
-      client.add(this.source.source, (t) => {
-        t.on("infohash", () => {
-          console.log("torrent", "infohash");
-        });
-        t.on("metadata", () => {
-          console.log("torrent", "metadata");
-        });
-        t.on("ready", () => {
-          console.log("torrent", "ready");
-        });
-        t.on("warning", (warn) => {
-          console.warn("torrent", warn);
-        });
-        t.on("error", (err) => {
-          console.error("torrent", err);
-        });
-        t.on("done", () => {
-          console.log("torrent", "done");
-        });
+      if (this.sources[this.sourceIndex]) {
+        client.add(this.sources[this.sourceIndex].source, (t) => {
+          t.on("infohash", () => {
+            console.log("torrent", "infohash");
+          });
+          t.on("metadata", () => {
+            console.log("torrent", "metadata");
+          });
+          t.on("ready", () => {
+            console.log("torrent", "ready");
+          });
+          t.on("warning", (warn) => {
+            console.warn("torrent", warn);
+          });
+          t.on("error", (err) => {
+            console.error("torrent", err);
+          });
+          t.on("done", () => {
+            console.log("torrent", "done");
+          });
 
-        // TODO support multiple video and audio codecs (mkv avi m4v...)
-        var f = t.files.find((f) => f.name.endsWith(".mp4") );
-        this.source.mime_type = "video/mp4";
+          // TODO support multiple video and audio codecs (mkv avi m4v...)
 
-        if (f) {
-          this.setStatus("Initializing video player");
+          // Filter files by those with supported file extensions
+          // then reduce down to the single largest file.
+          const f = t.files
+            .filter((f) =>
+              ["mp4"].some((ext) =>
+                f.name.endsWith("." + ext)
+              )
+            )
+            .reduce((a, b) => {
+              return a.length > b.length ? a : b;
+            });
 
-          this.player = videojs(
-            this.$refs.video.getAttribute("id"),
-            {
-              inactivityTimeout: 3000,
-            },
-            () => {
-              f.renderTo(`#${this.$refs.video.getAttribute("id")}`);
-              this.blob = this.$refs.video.getAttribute("src");
-            }
-          );
-
-          setTimeout(() => {
-            this.setStatus("Setting player blob");
-            this.player.cache_.source = {
-              type: this.source.mime_type,
-              src: this.blob,
-            };
-
-            this.client = client;
-            this.torrent = t;
-
-            this.setStatus("Streaming");
-
-            // Begin background fade
-            this.$refs.backdropElement.classList.add("hidden", "fade");
+          if (f) {
+            this.sources[this.sourceIndex].mime_type = "video/mp4";
+            this.setStatus("Initializing video player");
 
             setTimeout(() => {
-              this.$refs.video.parentNode.classList.remove("hidden");
-              this.$refs.video.classList.remove("hidden");
+              if (this.player.readyState() == 0) {
+                // Change the source type
+                this.sourceIndex++;
+                this.setStatus("Trying another source");
+              }
+            }, 13000);
 
-              this.loading = false;
-              this.player.play();
-            }, 3000); // create suspense... 1 second of blackness :)
-          }, 100);
-        } else {
-          this.setStatus("Can't find video file. Select another source.");
-        }
-      });
+            this.player = videojs(
+              this.$refs.video.getAttribute("id"),
+              function () {
+                f.renderTo(`#${this.$refs.video.getAttribute("id")}`);
+                this.blob = this.$refs.video.getAttribute("src");
+
+                setTimeout(() => {
+                  this.setStatus("Setting player blob");
+
+                  this.player.cache_.source = {
+                    type: this.source.mime_type,
+                    src: this.blob,
+                  };
+
+                  this.client = client;
+                  this.torrent = t;
+
+                  this.setStatus("Streaming");
+
+                  // Begin background fade
+                  this.$refs.backdropElement.classList.add("hidden", "fade");
+
+                  setTimeout(() => {
+                    this.$refs.video.parentNode.classList.remove("hidden");
+                    this.$refs.video.classList.remove("hidden");
+
+                    this.loading = false;
+                    this.player.play();
+
+                    setInterval(
+                      () =>
+                        console.log(
+                          t.progress * 100,
+                          (t.downloaded / 1024) ^
+                            (2 + "MB of " + t.length / 1024) ^
+                            2,
+                          ("@" + t.downloadSpeed / 1024) ^ (2 + "MB/s")
+                        ),
+                      5000
+                    );
+                  }, 3000); // create suspense... 1 second of blackness :)
+                }, 100);
+              }
+            );
+          } else {
+            this.setStatus(
+              "Video type is unsupported. Streaming to VLC instead."
+            );
+            console.log("Opening", t.path);
+            require("child_process").spawn("vlc", [t.path, "--fullscreen"]);
+            this.$router.back();
+          }
+        });
+      } else {
+        this.setStatus("Source unavailable. Check log for details.");
+      }
     },
     getBackdrop(i) {
-      var backdrop = this.backdrops[i].file_path || this.backdrops[i].url;
+      var backdrop = this.backdrops[i]
+        ? this.backdrops[i].file_path || this.backdrops[i].url
+        : "";
 
       if (backdrop.startsWith("/")) {
         backdrop = "https://image.tmdb.org/t/p/original" + backdrop;
@@ -346,7 +362,7 @@ export default {
 
       clearInterval(this.backdrop_interval);
       this.backdrop_interval = setInterval(() => {
-        if (this.loading) {
+        if (this.loading && this.$refs.backdropElement) {
           // hide
           this.$refs.backdropElement.classList.remove("show");
           this.$refs.backdropElement.classList.add("hidden", "fade");
@@ -381,6 +397,10 @@ export default {
     this.startRotatingBackdrops();
   },
   beforeUnmount() {
+    // Cancel all requests
+    this.cancelTokens.fanart.cancel("Operation canceled by lifecycle hook.");
+    this.cancelTokens.details.cancel("Operation canceled by lifecycle hook.");
+
     if (this.player) {
       try {
         this.player.dispose();
