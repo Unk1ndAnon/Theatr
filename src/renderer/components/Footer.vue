890102b6a3f0a1e8deba46b535aa6d395dd97f41
magic328 <50644033+magic328@users.noreply.github.com>
2021-06-09 01:37:20 -0700

    Create footer component

---
+++ src/renderer/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="footer">
+    <div class="columns">
+      <div class="col">
+        <h3 class="col-header">Links</h3>
+        <ul class="linklist">
+          <li>Link 1</li>
+        </ul>
+      </div>
+
+      <div class="col">
+        <h3 class="col-header">Links</h3>
+        <ul class="linklist">
+          <li>Link 1</li>
+        </ul>
+      </div>
+
+      <div class="col">
+        <h3 class="col-header">Links</h3>
+        <ul class="linklist">
+          <li>Link 1</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+div.footer {
+  width: 100%;
+  background: #101010;
+  padding: 10px 0 24px 0;
+
+  div.columns {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-evenly;
+
+    .col {
+      display: flex;
+      flex-flow: column;
+
+      .col-header { text-align: center; }
+    }
+  }
+
+  ul {
+      list-style-type: none;
+      padding: 0;
+  }
+}
+</style>
